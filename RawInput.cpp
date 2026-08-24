57ac63d220c5e9cd2a934bdc4f82c80d145e2b8d
radarblack <120578800+radarblack@users.noreply.github.com>
2026-08-25 06:10:06 +0800

    Added RawInput.cpp
    Refactor comments for clarity and consistency, and improve code formatting.

---
+++ RawInput.cpp
@@ -0,0 +1,447 @@
+//DEBUGNOW this only really gets you OnKeyDown, OnKeyUp reliably as Held will be limited by key repeat rate
+//the solution there would be to have another state array and have the input events set up,down and querry that with the assumption that down is held
+
+#include "RawInput.h"
+#include "spdlog/spdlog.h"
+
+namespace RadarKeys {
+	namespace RawInput {
+		const USHORT vKeyMax = 256; // virtual keycode max (VK_OEM_CLEAR      0xFE)
+		USHORT currFlags[vKeyMax]; // indexed by Virtual Keycode
+		bool ignore[vKeyMax] = { false }; // don't process key, set up in InitIgnoreKeys
+		bool blockGameKeys[vKeyMax] = { false }; // block game from recieving message
+		std::list<std::pair<ActionHandle, ButtonAction>>* buttonActions[vKeyMax] = { nullptr };
+		ActionHandle nextActionHandle = 1; // 0 reserved as an "invalid/none" sentinel if ever needed
+
+		void BlockMouseClick() {
+			blockGameKeys[VK_LBUTTON] = true;
+		}
+
+		void UnBlockMouseClick() {
+			blockGameKeys[VK_LBUTTON] = false;
+		}
+
+		void BlockAll() {
+			for (int i = 0; i < vKeyMax; i++) {
+				blockGameKeys[i] = true;
+			}
+		}
+
+		void UnBlockAll() {
+			for (int i = 0; i < vKeyMax; i++) {
+				blockGameKeys[i] = false;
+			}
+		}
+
+		void BlockKeyboard() {
+			for (USHORT i = VK_BACK; i < 256; i++) {
+				blockGameKeys[i] = true;
+			}
+			blockGameKeys[VK_ESCAPE] = false;
+		}
+
+		void UnBlockKeyboard() {
+			for (USHORT i = VK_BACK; i < 256; i++) {
+				blockGameKeys[i] = false;
+			}
+		}
+
+		USHORT gamepadKeys[]{
+			VK_GAMEPAD_A                         ,
+			VK_GAMEPAD_B                         ,
+			VK_GAMEPAD_X                         ,
+			VK_GAMEPAD_Y                         ,
+			VK_GAMEPAD_RIGHT_SHOULDER            ,
+			VK_GAMEPAD_LEFT_SHOULDER             ,
+			VK_GAMEPAD_LEFT_TRIGGER              ,
+			VK_GAMEPAD_RIGHT_TRIGGER             ,
+			VK_GAMEPAD_DPAD_UP                   ,
+			VK_GAMEPAD_DPAD_DOWN                 ,
+			VK_GAMEPAD_DPAD_LEFT                 ,
+			VK_GAMEPAD_DPAD_RIGHT                ,
+			VK_GAMEPAD_MENU                      ,
+			VK_GAMEPAD_VIEW                      ,
+			VK_GAMEPAD_LEFT_THUMBSTICK_BUTTON    ,
+			VK_GAMEPAD_RIGHT_THUMBSTICK_BUTTON   ,
+			VK_GAMEPAD_LEFT_THUMBSTICK_UP        ,
+			VK_GAMEPAD_LEFT_THUMBSTICK_DOWN      ,
+			VK_GAMEPAD_LEFT_THUMBSTICK_RIGHT     ,
+			VK_GAMEPAD_LEFT_THUMBSTICK_LEFT      ,
+			VK_GAMEPAD_RIGHT_THUMBSTICK_UP       ,
+			VK_GAMEPAD_RIGHT_THUMBSTICK_DOWN     ,
+			VK_GAMEPAD_RIGHT_THUMBSTICK_RIGHT    ,
+			VK_GAMEPAD_RIGHT_THUMBSTICK_LEFT     ,
+		};
+
+		void DoActions(USHORT vKey, RawInput::BUTTONEVENT buttonEvent);
+
+		void ProcessKey(PRAWINPUT pRaw) {
+			//spdlog::trace("ProcessKey");//DEBUG
+			USHORT vKey = pRaw->data.keyboard.VKey;
+			USHORT flags = pRaw->data.keyboard.Flags;
+			USHORT oldFlags = currFlags[vKey];
+
+			BUTTONEVENT buttonEvent = BUTTONEVENT::UP;
+			if (flags == RI_KEY_MAKE && oldFlags == RI_KEY_BREAK) {//OnKeyDown
+				buttonEvent = BUTTONEVENT::ONDOWN;
+			}
+			else if (flags == RI_KEY_BREAK && oldFlags == RI_KEY_MAKE) {//OnKeyUp
+				buttonEvent = BUTTONEVENT::ONUP;
+			}
+			else if (flags == RI_KEY_MAKE && oldFlags == RI_KEY_MAKE) {//Held
+				buttonEvent = BUTTONEVENT::HELD;
+			}
+			//else up, which you shouldnt hit
+
+			currFlags[vKey] = flags;
+
+			DoActions(vKey, buttonEvent);
+
+#ifdef _DEBUG
+			//WCHAR wcTextBuffer[512];
+			//UINT keyChar = MapVirtualKey(pRaw->data.keyboard.VKey, MAPVK_VK_TO_CHAR);
+
+			//wsprintf(wcTextBuffer,
+			//	TEXT("Type=%d\nDevice=0x%x\nMakeCode=0x%x\nFlags=0x%x\nReserved=0x%x\nExtraInformation=0x%x\nMessage=0x%x\nVKey=0x%x\nEvent=0x%x\nkeyChar=0x%x\n\n"),
+			//	/// device header
+			//	pRaw->header.dwType,
+			//	// device handle, pass this to GetRawInputDeviceInfo
+			//	pRaw->header.hDevice,
+
+			//	pRaw->data.keyboard.MakeCode,
+			//	pRaw->data.keyboard.Flags,
+			//	pRaw->data.keyboard.Reserved,
+			//	pRaw->data.keyboard.ExtraInformation,
+			//	pRaw->data.keyboard.Message,
+			//	pRaw->data.keyboard.VKey,
+			//	keyChar);
+
+			//wprintf(wcTextBuffer);
+#endif // _DEBUG
+		}//ProcessRawInput
+
+		// helper to process usButtonFlags
+		struct {
+			USHORT vk;		UINT downflag;					UINT upflag;
+		} const k[] = {
+			{ VK_LBUTTON,   RI_MOUSE_LEFT_BUTTON_DOWN,		RI_MOUSE_LEFT_BUTTON_UP },
+			{ VK_RBUTTON,   RI_MOUSE_RIGHT_BUTTON_DOWN,		RI_MOUSE_RIGHT_BUTTON_UP },
+			{ VK_MBUTTON,	RI_MOUSE_MIDDLE_BUTTON_DOWN,	RI_MOUSE_MIDDLE_BUTTON_UP },
+			{ VK_XBUTTON1,	RI_MOUSE_BUTTON_4_DOWN,			RI_MOUSE_BUTTON_4_UP },
+			{ VK_XBUTTON2,	RI_MOUSE_BUTTON_5_DOWN,			RI_MOUSE_BUTTON_4_UP }
+		};
+
+		bool ProcessMouseButtons(PRAWINPUT pRaw) {
+			USHORT usButtonFlags = pRaw->data.mouse.usButtonFlags;
+
+			const int numButtons = _countof(k);
+
+			// jump through a few hoops to make it similar to ProcessKey
+			USHORT oldFlagsB[vKeyMax];
+			for (UINT i = 0; i < numButtons; ++i) {
+				USHORT vKey = k[i].vk;
+				oldFlagsB[vKey] = currFlags[vKey];
+			}
+
+			for (UINT i = 0; i < numButtons; ++i) {
+				USHORT vKey = k[i].vk;
+				if (usButtonFlags & k[i].downflag) {
+					currFlags[vKey] = RI_KEY_MAKE;
+				}
+				//DEBUGNOW not hitting for some reason
+				if (usButtonFlags & k[i].upflag) {
+					currFlags[vKey] = RI_KEY_BREAK;
+				}
+			}
+			//
+
+			for (UINT i = 0; i < numButtons; ++i) {
+				USHORT vKey = k[i].vk;
+				USHORT flags = currFlags[vKey];
+				USHORT oldFlags = oldFlagsB[vKey];
+
+				BUTTONEVENT buttonEvent = BUTTONEVENT::UP;
+				if (flags == RI_KEY_MAKE && oldFlags == RI_KEY_BREAK) {
+					buttonEvent = BUTTONEVENT::ONDOWN;
+				}
+				else if (flags == RI_KEY_BREAK && oldFlags == RI_KEY_MAKE) {
+					buttonEvent = BUTTONEVENT::ONUP;
+				}
+				else if (flags == RI_KEY_MAKE && oldFlags == RI_KEY_MAKE) {
+					buttonEvent = BUTTONEVENT::HELD;
+				}
+
+				if (blockGameKeys[vKey]) {
+					return false;
+				}
+
+				if (!ignore[vKey]) {
+					DoActions(vKey, buttonEvent);
+				}
+			}//for numbuttons
+
+#ifdef _DEBUG
+		//WCHAR wcTextBuffer[512];
+
+		//wsprintf(wcTextBuffer,
+		//	TEXT("Type=%d\nDevice=0x%x\nulButtons=0x%x\nulRawButtons=0x%x\nusButtonData=0x%x\nusButtonFlags=0x%x\nusFlags=0x%x\nlLastX=0x%x\nlLastY=0x%x\n\n"),
+		//	pRaw->header.dwType,
+		//	pRaw->header.hDevice,
+
+		//	pRaw->data.mouse.ulButtons,
+		//	pRaw->data.mouse.ulRawButtons,
+		//	pRaw->data.mouse.usButtonData,
+		//	pRaw->data.mouse.usButtonFlags,
+		//	pRaw->data.mouse.usFlags,
+		//	pRaw->data.mouse.lLastX,
+		//	pRaw->data.mouse.lLastY);
+
+		//wprintf(wcTextBuffer);
+#endif // _DEBUG
+			return true;
+		}//ProcessMouseButtons
+
+		//IN/SIDE: buttonActions
+		void DoActions(USHORT vKey, RawInput::BUTTONEVENT buttonEvent) {
+			std::list<std::pair<ActionHandle, ButtonAction>>* actions = buttonActions[vKey];
+			if (actions != nullptr) {
+				spdlog::debug("RawInput DoActions for vKey:{}", vKey);
+				for (auto it = actions->begin(); it != actions->end(); ++it) {
+					ButtonAction Action = it->second;
+					Action(buttonEvent);
+				}
+			}
+		}//DoActions
+
+
+		ActionHandle RegisterAction(USHORT vKey, ButtonAction action) {
+			assert(vKey > 0 && vKey < vKeyMax);
+			spdlog::debug("RawInput RegisterAction for vKey:{}", vKey);
+			if (buttonActions[vKey] == nullptr) {
+				buttonActions[vKey] = new std::list<std::pair<ActionHandle, ButtonAction>>();
+			}
+
+			ActionHandle handle = nextActionHandle++;
+			buttonActions[vKey]->push_back({ handle, action });
+			return handle;
+		}//RegisterAction
+
+		void UnRegisterAction(USHORT vKey) {
+			if (buttonActions[vKey] == nullptr) {
+				spdlog::warn("RawInput UnRegisterAction: No actions for vKey {}", vKey);
+				return;
+			}
+			else { // HWL TODO: haven't checked if this works properly
+				delete buttonActions[vKey];
+				buttonActions[vKey] = nullptr;
+			}
+		}//UnRegisterAction
+
+		void UnRegisterAction(USHORT vKey, ActionHandle handle) {
+			std::list<std::pair<ActionHandle, ButtonAction>>* actions = buttonActions[vKey];
+			if (actions == nullptr) {
+				spdlog::warn("RawInput UnRegisterAction: No actions for vKey {}", vKey);
+				return;
+			}
+			for (auto it = actions->begin(); it != actions->end(); ++it) {
+				if (it->first == handle) {
+					actions->erase(it);
+					spdlog::debug("RawInput UnRegisterAction: removed handle {} from vKey {}", handle, vKey);
+					if (actions->empty()) {
+						delete buttonActions[vKey];
+						buttonActions[vKey] = nullptr;
+					}
+					return;
+				}
+			}
+			spdlog::warn("RawInput UnRegisterAction: handle {} not found for vKey {}", handle, vKey);
+		}//UnRegisterAction (handle)
+
+		bool IsKeyDown(USHORT vKey) {
+			return currFlags[vKey] == RI_KEY_MAKE;
+		}//IsKeyDown
+
+		//DEBUG
+		void TestAction(BUTTONEVENT buttonEvent) {
+			spdlog::debug("ButtonEvent: {:d}, Action: TestAction", buttonEvent);
+			if (buttonEvent == BUTTONEVENT::ONDOWN) {
+				spdlog::debug("TestAction on ONDOWN");
+			}
+			else if (buttonEvent == BUTTONEVENT::ONUP) {
+				spdlog::debug("TestAction on ONUP");
+			}
+			else  if (buttonEvent == BUTTONEVENT::HELD) {
+				spdlog::debug("TestAction on HELD");
+			}
+		}//TestAction
+
+		//tex: don't process key //DEBUGNOW what am I doing here?
+		void InitIgnoreKeys() {
+			ignore[VK_KANA] = true;
+			ignore[VK_HANGEUL] = true;
+			ignore[VK_HANGUL] = true;
+			ignore[VK_JUNJA] = true;
+			ignore[VK_FINAL] = true;
+			ignore[VK_HANJA] = true;
+			ignore[VK_KANJI] = true;
+			ignore[VK_CONVERT] = true;
+			ignore[VK_NONCONVERT] = true;
+			ignore[VK_ACCEPT] = true;
+			ignore[VK_MODECHANGE] = true;
+
+			ignore[VK_SLEEP] = true;
+			ignore[VK_NAVIGATION_VIEW] = true;
+			ignore[VK_NAVIGATION_MENU] = true;
+			ignore[VK_NAVIGATION_UP] = true;
+			ignore[VK_NAVIGATION_DOWN] = true;
+			ignore[VK_NAVIGATION_LEFT] = true;
+			ignore[VK_NAVIGATION_RIGHT] = true;
+			ignore[VK_NAVIGATION_ACCEPT] = true;
+			ignore[VK_NAVIGATION_CANCEL] = true;
+
+			ignore[VK_OEM_NEC_EQUAL] = true;
+
+
+			ignore[VK_OEM_FJ_JISHO] = true;
+			ignore[VK_OEM_FJ_MASSHOU] = true;
+			ignore[VK_OEM_FJ_TOUROKU] = true;
+			ignore[VK_OEM_FJ_LOYA] = true;
+			ignore[VK_OEM_FJ_ROYA] = true;
+
+			ignore[VK_OEM_AX] = true;
+			ignore[VK_OEM_102] = true;
+			ignore[VK_ICO_HELP] = true;
+			ignore[VK_ICO_00] = true;
+
+			ignore[VK_PROCESSKEY] = true;
+			ignore[VK_ICO_CLEAR] = true;
+			ignore[VK_PACKET] = true;
+
+			ignore[VK_OEM_RESET] = true;
+			ignore[VK_OEM_JUMP] = true;
+			ignore[VK_OEM_PA1] = true;
+			ignore[VK_OEM_PA2] = true;
+			ignore[VK_OEM_PA3] = true;
+			ignore[VK_OEM_WSCTRL] = true;
+			ignore[VK_OEM_CUSEL] = true;
+			ignore[VK_OEM_ATTN] = true;
+			ignore[VK_OEM_FINISH] = true;
+			ignore[VK_OEM_COPY] = true;
+			ignore[VK_OEM_AUTO] = true;
+			ignore[VK_OEM_ENLW] = true;
+			ignore[VK_OEM_BACKTAB] = true;
+
+			ignore[VK_ATTN] = true;
+			ignore[VK_CRSEL] = true;
+			ignore[VK_EXSEL] = true;
+			ignore[VK_EREOF] = true;
+			ignore[VK_PLAY] = true;
+			ignore[VK_ZOOM] = true;
+			ignore[VK_NONAME] = true;
+			ignore[VK_PA1] = true;
+			ignore[VK_OEM_CLEAR] = true;
+		}//InitIgnoreKeys
+
+		void InitializeInput() {
+			spdlog::debug("Rawinput InitializeInput");
+
+			std::fill_n(currFlags, vKeyMax, static_cast<USHORT>(RI_KEY_BREAK));
+
+			InitIgnoreKeys();
+
+			// No fixed actions; dynamically registers KeyBindMenu/DebuggerMenu via RegisterAction (F4 toggle + persisted bindings).
+		}
+
+		//CULL not needed, the game will have set up it's own
+		//Could use it to allow funky controllers though
+		void InitializeRawInputDevices() {
+			RAWINPUTDEVICE Rid[2];
+
+			Rid[0].usUsagePage = 0x01;
+			Rid[0].usUsage = 0x02;
+			// Rid[0].dwFlags = RIDEV_NOLEGACY;   // adds HID mouse and also ignores legacy mouse messages
+			Rid[0].hwndTarget = 0;
+
+			Rid[1].usUsagePage = 0x01;
+			Rid[1].usUsage = 0x06;
+			// Rid[1].dwFlags = RIDEV_NOLEGACY;   // adds HID keyboard and also ignores legacy keyboard messages
+			Rid[1].hwndTarget = 0;
+
+			if (RegisterRawInputDevices(Rid, 2, sizeof(Rid[0])) == FALSE) {
+				spdlog::warn("register raw input devices failed {}", GetLastError());
+			}
+
+		}//InitializeRawInput
+
+		bool OnMessage(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+			switch (uMsg) {
+			case WM_INPUT:
+			{
+				// wParam is either RIM_INPUT (this app foreground) or RIM_INPUTSINK (this app background)
+				// lParam is the RAWINPUT handle
+
+				UINT dwSize;
+
+				// determine size of buffer
+				if (GetRawInputData((HRAWINPUT)lParam, RID_INPUT, NULL, &dwSize, sizeof(RAWINPUTHEADER)) == -1) {
+					break;
+				}
+
+				LPBYTE lpb = new BYTE[dwSize];
+				if (lpb == NULL) {
+					break;
+				}
+				ZeroMemory(lpb, dwSize);
+
+				// get actual data
+				if (GetRawInputData((HRAWINPUT)lParam, RID_INPUT, lpb, &dwSize, sizeof(RAWINPUTHEADER)) != dwSize) {
+					delete[] lpb;
+					break;
+				}
+
+				// process it
+				PRAWINPUT pRaw = (PRAWINPUT)lpb;
+				if (pRaw->header.dwType == RIM_TYPEKEYBOARD) {
+					USHORT vKey = pRaw->data.keyboard.VKey;
+					if (blockGameKeys[vKey]) {
+						delete[] lpb;
+						return false;
+					}
+
+					if (!ignore[vKey]) {
+						ProcessKey(pRaw);
+					}
+				}
+				else if (pRaw->header.dwType == RIM_TYPEMOUSE) {
+					if (!ProcessMouseButtons(pRaw)) {
+						delete[] lpb;
+						return false;
+					}
+				}
+
+				// not needed
+				delete[] lpb;
+				break;
+			}//case WM_INPUT
+			}//switch uMsg
+
+			return true;
+		}//OnMessage
+
+		//
+		WNDPROC WndProc_Orig = NULL;
+
+		LRESULT CALLBACK WndProc_Hook(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+			if (!OnMessage(hwnd, uMsg, wParam, lParam)) {
+				return -1L;
+			}
+
+			return CallWindowProc(WndProc_Orig, hwnd, uMsg, wParam, lParam);
+		}//WndProc_Hook
+
+		void HookWndProc(HWND hWnd) {
+			//Redirect WndProc for hWnd
+			WndProc_Orig = (WNDPROC)SetWindowLongPtr(hWnd, GWLP_WNDPROC, (LONG_PTR)WndProc_Hook);
+		}
+	}//namespace RawInput
+}//namespace RadarKeys
