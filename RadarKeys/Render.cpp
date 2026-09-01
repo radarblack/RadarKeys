4521d7af090d415301a8a8ec47cdc6bd4b60d7d4
radarblack <120578800+radarblack@users.noreply.github.com>
2026-09-01 13:55:16 +0800

    Refactor condition for handling WM_INPUT message

--- RadarKeys/Render.cpp
+++ RadarKeys/Render.cpp
@@ -67,8 +67,8 @@ namespace RadarKeys {
 			}
 
 			if (message == WM_INPUT) {
-				if (IsUnlockCursor() || KeyBindMenu::showCapturePrompt) {
-					return false; // swallows packet completely
+				if (IsUnlockCursor() || showCapturePrompt) {
+					return false;
 				}
 			}
 
