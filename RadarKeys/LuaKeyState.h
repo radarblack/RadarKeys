a62c008f1a88d643f9f3657d0b67190285d7776e
radarblack <120578800+radarblack@users.noreply.github.com>
2026-09-03 11:33:30 +0800

    Update LuaKeyState.h

--- RadarKeys/LuaKeyState.h
+++ RadarKeys/LuaKeyState.h
@@ -6,8 +6,8 @@ namespace RadarKeys {
 		bool ButtonDown(USHORT vKey);
 		bool OnButtonDown(USHORT vKey);
 		bool OnButtonUp(USHORT vKey);
-		bool ButtonHeld(USHORT vKey);
-		bool OnButtonHoldTime(USHORT vKey);
+		bool ButtonHeld(USHORT vKey, double holdSecondsOverride = -1.0);
+		bool OnButtonHoldTime(USHORT vKey, double holdSecondsOverride = -1.0);
 		bool OnButtonRepeat(USHORT vKey);
 		double GetRepeatMult(USHORT vKey);
 		void ResetRepeat(USHORT vKey);
