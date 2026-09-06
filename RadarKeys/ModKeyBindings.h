6c4fa802448bc2d27598fa3e04908c0db770d51f
radarblack <120578800+radarblack@users.noreply.github.com>
2026-09-06 09:10:41 +0800

    Add OverrideEntry struct and related functions

--- RadarKeys/ModKeyBindings.h
+++ RadarKeys/ModKeyBindings.h
@@ -1,10 +1,20 @@
 #pragma once
 #include <string>
+#include <vector>
 
 namespace RadarKeys {
 	namespace ModKeyBindings {
+		struct OverrideEntry {
+			std::string scriptName;
+			std::string functionName;
+			std::string keyName;
+		};
+
 		void Load();
 		std::string GetOverride(const std::string& scriptName, const std::string& functionName);
 		void SetOverride(const std::string& scriptName, const std::string& functionName, const std::string& keyName);
+
+		std::vector<OverrideEntry> GetAllOverrides();
+		void LoadFromEntries(const std::vector<OverrideEntry>& entries);
 	}
 }
