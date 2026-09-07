3ea54af1c3b8e5697ea16391aafda50e89069fb6
radarblack <120578800+radarblack@users.noreply.github.com>
2026-09-07 13:11:44 +0800

    Add physical button state functions to LuaKeyState

--- RadarKeys/LuaKeyState.h
+++ RadarKeys/LuaKeyState.h
@@ -8,8 +8,11 @@ namespace RadarKeys {
 		bool ButtonDown(USHORT vKey);
 		bool OnButtonDown(USHORT vKey);
 		bool OnButtonUp(USHORT vKey);
+		bool PhysicalOnButtonDown(USHORT vKey);
+		bool PhysicalOnButtonUp(USHORT vKey);
 		bool ButtonHeld(USHORT vKey, double holdSecondsOverride = -1.0);
 		bool OnButtonHoldTime(USHORT vKey, double holdSecondsOverride = -1.0);
+		bool PhysicalOnButtonHoldTime(USHORT vKey, double holdSecondsOverride = -1.0);
 		bool OnButtonRepeat(USHORT vKey);
 		double GetRepeatMult(USHORT vKey);
 		void ResetRepeat(USHORT vKey);
