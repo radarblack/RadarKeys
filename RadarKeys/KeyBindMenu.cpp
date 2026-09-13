085bdd26dfcd1fd631721915854f0102e961f132
radarblack <120578800+radarblack@users.noreply.github.com>
2026-09-13 13:00:43 +0800

    Refactor key press detection logic in KeyBindMenu

--- RadarKeys/KeyBindMenu.cpp
+++ RadarKeys/KeyBindMenu.cpp
@@ -1679,10 +1679,11 @@ namespace RadarKeys {
 							if (i == VK_LBUTTON && ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow)) continue;
 							if (ImGui::IsKeyPressed((ImGuiKey)i)) { pressedKey = (USHORT)i; break; }
 						} 
-						if (pressedKey == 0) {
-						   for (USHORT gpKey : RawInput::GamepadVKeys()) {
-						      if (RawInput::IsKeyHeldReal(gpKey)) { pressedKey = gpKey; break; }
-					    }
+							if (pressedKey == 0) {
+							   for (USHORT gpKey : RawInput::GamepadVKeys()) {
+							      if (RawInput::IsKeyHeldReal(gpKey)) { pressedKey = gpKey; break; }
+						    }
+						}
 					}
 					if (pressedKey != 0) {
 						singleHoldKey = pressedKey;
