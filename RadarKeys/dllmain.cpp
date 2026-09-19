f197dbf426450040f53bf440a8428b5caa17b657
radarblack <120578800+radarblack@users.noreply.github.com>
2026-09-19 11:00:38 +0800

    Refactor DirectInputHook installation call

--- RadarKeys/dllmain.cpp
+++ RadarKeys/dllmain.cpp
@@ -309,7 +309,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 	case DLL_PROCESS_ATTACH:
 		DisableThreadLibraryCalls(hModule);
 		MH_Initialize();
-		RadarKeys::DirectInputHook::Install();
+		DirectInputHook::Install();
 		std::thread(RadarKeys::InitThread).detach();
 		break;
 	case DLL_PROCESS_DETACH:
