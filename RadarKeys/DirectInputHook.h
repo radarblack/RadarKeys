30456999197e75c7876e3745118ac570dbc8a341
radarblack <120578800+radarblack@users.noreply.github.com>
2026-09-20 15:12:20 +0800

    Remove unused function PlaystationVocabLatched
    Removed unused function PlaystationVocabLatched from DirectInputHook.h

--- RadarKeys/DirectInputHook.h
+++ RadarKeys/DirectInputHook.h
@@ -11,7 +11,6 @@ namespace RadarKeys {
 		bool IsPlaystationControlHeld(USHORT vKey);
 		bool HasJoystickDevice();
 		bool HasPlaystationDevice();
-		bool PlaystationVocabLatched();
 		bool IsSonyGamepadAttachedToSystem();
 	}
 }
