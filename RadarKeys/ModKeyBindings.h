5a28d07e1fa65b4ac81811f6067ce34ca2e708da
radarblack <120578800+radarblack@users.noreply.github.com>
2026-09-22 11:43:52 +0800

    Add BindSlot enum and padKeyName to OverrideEntry

--- RadarKeys/ModKeyBindings.h
+++ RadarKeys/ModKeyBindings.h
@@ -4,17 +4,26 @@
 
 namespace RadarKeys {
 	namespace ModKeyBindings {
+		enum class BindSlot {
+			Kbm = 0,
+			Pad = 1
+		};
+
 		struct OverrideEntry {
 			std::string scriptName;
 			std::string functionName;
 			std::string keyName;
+			std::string padKeyName;
 			bool disabled = false;
 		};
 
 		void Load();
 		std::string GetOverride(const std::string& scriptName, const std::string& functionName);
+		std::string GetSlotOverride(const std::string& scriptName, const std::string& functionName, BindSlot slot);
 		void SetOverride(const std::string& scriptName, const std::string& functionName, const std::string& keyName);
 		void SetOverrideWithoutSave(const std::string& scriptName, const std::string& functionName, const std::string& keyName);
+		void SetSlotOverride(const std::string& scriptName, const std::string& functionName, BindSlot slot, const std::string& keyName);
+		void SetSlotOverrideWithoutSave(const std::string& scriptName, const std::string& functionName, BindSlot slot, const std::string& keyName);
 		void SetDisabledWithoutSave(const std::string& scriptName, const std::string& functionName, bool disabled);
 
 		bool IsDisabled(const std::string& scriptName, const std::string& functionName);
