bb2d87c9e2fe9484c124351b469afa0c2674b642
radarblack <120578800+radarblack@users.noreply.github.com>
2026-09-26 16:10:44 +0800

    Add methods to get stale sweep seconds in LuaKeyState

--- RadarKeys/LuaKeyState.h
+++ RadarKeys/LuaKeyState.h
@@ -40,6 +40,8 @@ namespace RadarKeys {
 		void DescribeKey(USHORT vKey, const std::string& scriptName, const std::string& functionName, const std::string& toggleState);
 		void DescribeComboKey(const std::vector<USHORT>& vKeys, const std::string& scriptName, const std::string& functionName, const std::string& toggleState);
 		void SweepStaleComboDescriptions();
+		double GetStaleSweepSecondsRemaining();
+		double GetStaleSweepSecondsTotal();
 		void OnFocusLost();
 		USHORT FindRedirectSource(USHORT activeVKey);
 		void ClearComboRedirect(const std::vector<USHORT>& nativeVKeys);
